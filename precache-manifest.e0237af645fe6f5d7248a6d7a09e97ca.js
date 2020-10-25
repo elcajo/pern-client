c10e36d601984ebf7d9c2f620f205f17a84131f2
elcajo <46391075+elcajo@users.noreply.github.com>
2020-10-25 18:36:00 +0800

    Updates

---
+++ precache-manifest.e0237af645fe6f5d7248a6d7a09e97ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72eb347ea0010c88a67751235f8c3608",
+    "url": "/pern-client/index.html"
+  },
+  {
+    "revision": "d1858cd6c720fc325dfb",
+    "url": "/pern-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b153248b94bb536f48ee",
+    "url": "/pern-client/static/js/2.4d2939bb.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/pern-client/static/js/2.4d2939bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1858cd6c720fc325dfb",
+    "url": "/pern-client/static/js/main.bb094e05.chunk.js"
+  },
+  {
+    "revision": "bcc27e6cfe2073f3dafe",
+    "url": "/pern-client/static/js/runtime-main.f8fbd1ca.js"
+  }
+]);
